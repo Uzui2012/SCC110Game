ccb7a2a9256b9272ed319bf700db86b0cedfee55
Hokari-Daly <hokarida@egb080000028.lancs.local>
2020-02-14 16:35:35 +0000

    Ive fucked up the code... save me killian

--- Player.java
+++ Player.java
@@ -5,11 +5,12 @@ public class Player
     private double speed; //speed of the player
     private double acceleration; //acc of player
     //private String color;     //player colour
-    public Player(double x, double y, double speed, double acceleration){
+    public Player(double x, double y, double h, double w, double acceleration){
 
         xAxis = x;
         yAxis = y;
-        this.speed = speed;
+        this.h = h;
+        this.w = w;
         this.acceleration = acceleration;
     }
     //obtain current x position of player
@@ -27,6 +28,28 @@ public class Player
     //set y position of player
     public void setY(double y){
         this.yAxis = y;
+    }
+    public double getHeight(){
+        return Height;
+    }
+
+    public void setHight(double h){
+        Height = h;
+    }
+
+    public double getWidth(){
+        return Width;
+    }
+
+    public void setWidth(double w){
+        Width = w;
+    }
+
+    public double getAcceleration(){
+        return Acceleration;
+    }
+    public void setAcceleration(double a){
+        Acceleration = a;
     }
 
 }
