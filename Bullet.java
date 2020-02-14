57fa734322f90d43a0181475ae5de58add9036f4
= <=>
2020-02-14 14:43:43 +0000

    Added draw method to Bullet class

--- Bullet.java
+++ Bullet.java
@@ -5,6 +5,9 @@ public class Bullet
     private int speed;              // The speed of the bullet
     private String colour;          // Colour of the bullet
     private int orientation;        // Orientation of the bullet, will be the same as the player
+    private GameArena gameArena;
+    private double width;
+    private double height;
 
     /** 
      * Create new instance of Bullet 
@@ -12,11 +15,12 @@ public class Bullet
      * @param y The Y Coordinate of the Bullet, in pixels
      * @param speed The speed of the Bullet
     */
-    public Bullet(double x, double y, int speed) {
+    public Bullet(double x, double y, int speed, GameArena ga) {
         xPosition = x;
         yPosition = y;
         this.speed = speed;
         colour = "darkgrey";
+        gameArena = ga;
     }
 
     public double getXPosition() {
@@ -51,7 +55,28 @@ public class Bullet
         colour = c;
     }
 
+    public void setWidth(int w) {
+        width = w;
+    }
+
+    public double getWidth() {
+        return width;
+    } 
+    
+    public void setHeight(int h) {
+        height = h;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
     /*public void addTo(GameArena ga) {
         // Add rectangles
     }*/
+
+    public void draw() {
+        Rectangle r = new Rectangle(xPosition, yPosition, width, height, colour);
+        gameArena.addRectangle(r);
+    }
 }
